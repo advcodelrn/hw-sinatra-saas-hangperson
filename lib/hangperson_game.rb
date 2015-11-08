dae728e879e1349dfa6ca8a4bb1ce0c6745faecc
advcodelrn <advstud@mail.com>
2015-11-08 21:39:48 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,12 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -19,4 +22,38 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess(letter)
+    raise ArgumentError if (letter.nil? || 
+                            letter.empty? || 
+                            !letter.match(/^[[:alpha:]]+$/))
+    # return false if the letter is already has been guessed
+    letter = letter.downcase
+    return false if (@guesses + @wrong_guesses).include? letter
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    true
+  end
+  
+  def word_with_guesses
+    @word.each_char.map do |letter|
+      if @guesses.include? letter
+        letter
+      else
+        '-'
+      end
+    end.join
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length > 6
+      return :lose
+    elsif @word.each_char.all? { |letter| @guesses.include? letter }
+      return :win
+    else
+      return :play
+    end
+  end
 end
